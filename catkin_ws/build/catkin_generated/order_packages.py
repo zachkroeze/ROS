b13669d613e55006f2b6bde94fc7dd27fe22a6e3
Zachary Kroeze <zach.kroeze@gmail.com>
2017-10-04 09:21:44 -0400

    Worked up to roscpp talker, listener

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/zach/ROS_Projects/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/zach/ROS/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/zach/ROS/catkin_ws/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/home/zach/ROS_Projects/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/zach/ROS_Projects/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,8 +7,5 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/zach/ROS_Projects/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/zach/ROS_Projects/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
 export PWD="/home/zach/ROS_Projects/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/zach/ROS_Projects/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/zach/ROS_Projects/catkin_ws/src:$ROS_PACKAGE_PATH"
+export PYTHONPATH="/home/zach/ROS_Projects/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
